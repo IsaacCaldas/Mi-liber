7981ed7fc3c58c0deec17fe9a5c4f085d9a7a685
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-05-18 20:59:08 -0300

    Added other routes

--- mi-liber-api/config/routes.rb
+++ mi-liber-api/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   default_url_options :host => "localhost:3000"
 
-  resources :users
-  resources :roles
+  resources :roles, defaults: { format: :json }
+  resources :users, defaults: { format: :json } 
+  resources :categories, defaults: { format: :json }
+  resources :books, defaults: { format: :json }
+  resources :user_book, defaults: { format: :json }
 
   root 'main#index'
 
